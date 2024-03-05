3d2b9a1c6b1af0fc78029e52220b69f945c3fbff
김형석 <104004779+kimhyoungseok@users.noreply.github.com>
2024-03-06 08:30:38 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 echo "start jar"
 echo "1. Get green image"
-docker-compose pull green
+sudo docker-compose pull green
 
 echo "2. Green container up"
-docker-compose up -d green
+sudo docker-compose up -d green
 
 echo "3. Reload nginx"
 sudo cp /etc/nginx/nginx.green.conf /etc/nginx/nginx.conf
